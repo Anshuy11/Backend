7be4b042ec7a39c9df7cf048da88183447e71473
Anshu <anshuy115@gmail.com>
2024-05-04 09:21:53 +0530

    data base connected

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+
+const app = express()
+app.use(cors({
+    origin : process.env.CROS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit: "16kb"}))  
+export {app}
